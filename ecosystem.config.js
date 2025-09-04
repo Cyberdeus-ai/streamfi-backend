243be7de83fd1773282ae5a2866846da006f871a
Cyberdeus-ai <Cyberdeus.ai@icloud.com>
2025-09-05 02:57:35 +0800

    fix: Updated ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
             host: '84.32.22.56',
             ref: 'origin/main',
             repo: 'https://github.com/Cyberdeus-ai/streamfi-backend.git',
-            path: '~/streamfi-backend',
+            path: '/root/streamfi-backend',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --name BACKEND'
         },
     },
