9103085cab044d17822234ec669e5ee73f16853d
Cyberdeus-ai <Cyberdeus.ai@icloud.com>
2025-09-05 02:17:17 +0800

    fix: Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,56 +1,19 @@
 module.exports = {
-  apps: [
-    // Express Backend
-    {
-      name: 'streamfi-backend',
-      cwd: '~/streamfi-backend',
-      script: 'npm',
-      args: 'run dev',
-      repo: 'git@github.com/Cyberdeus-ai/streamfi-backend.git',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5000,
-        DATABASE_URL: 'postgres://postgres:password@84.32.22.56:5432/template1',
-      },
-      autorestart: true,
-      watch: true,
-      max_memory_restart: '1G',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    },
-    // Next.js Frontend
-    {
-      name: 'streamfi-frontend',
-      cwd: '~/streamfi-frontend',
-      script: 'npm',
-      args: 'run dev',
-      repo: 'git@github.com/Cyberdeus-ai/streamfi-frontend.git',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 3000,
-      },
-      autorestart: true,
-      watch: true,
-      max_memory_restart: '1G',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    },
-  ],
+    apps: [
+        {
+            name: 'streamfi-backend',
+            script: 'npm run dev'
+        }
+    ],
 
-  deploy: {
-    production: {
-      'user': 'root',
-      'host': '84.32.22.56',
-      'ref': 'origin/main',
-      'repo': 'git@github.com/Cyberdeus-ai/streamfi-backend.git',
-      'path': '~/streamfi-backend',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js'
+    deploy: {
+        production: {
+            user: 'root',
+            host: '84.32.22.56',
+            ref: 'origin/main',
+            repo: 'https://github.com/Cyberdeus-ai/streamfi-backend.git',
+            path: '~/streamfi-backend',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --name BACKEND'
+        },
     },
-    frontend: {
-      'user': 'root',
-      'host': '84.32.22.56',
-      'ref': 'origin/main',
-      'repo': 'git@github.com/Cyberdeus-ai/streamfi-frontend.git',
-      'path': '~/streamfi-frontend',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js'
-    },
-  },
 };
