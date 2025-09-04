38460a6a1991eebbdf3c974977f65ecfc2d57294
Cyberdeus-ai <Cyberdeus.ai@icloud.com>
2025-09-04 23:37:44 +0800

    fit: Created ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  apps: [
+    // PostgreSQL Database Server
+    {
+      name: 'postgresql-server',
+      script: 'psql',
+      args: '--host 84.32.22.56 --username postgres --password --dbname template1',
+      env: {
+        NODE_ENV: 'production',
+      },
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+    },
+    // Express Backend
+    {
+      name: 'streamfi-backend',
+      cwd: '/streamfi-backend',
+      script: 'npm',
+      args: 'run dev',
+      repo: 'https://github.com/Cyberdeus-ai/streamfi-backend.git',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 5000,
+        DATABASE_URL: 'postgres://postgres:password@84.32.22.56:5432/template1',
+      },
+      autorestart: true,
+      watch: true, 
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+    // Next.js Frontend
+    {
+      name: 'streamfi-frontend',
+      cwd: '/streamfi-frontend',
+      script: 'npm',
+      args: 'run dev',
+      repo: 'https://github.com/Cyberdeus-ai/streamfi-frontend.git',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+};
