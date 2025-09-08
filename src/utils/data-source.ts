da715fba32b24fc1f054681be6f4a4285c2331a5
Cyberdeus-ai <Cyberdeus.ai@icloud.com>
2025-09-08 22:08:53 +0800

    fix: Updated data-source

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -1,14 +1,15 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User, Campaign, Post, Score, XAccount, Continuation } from '../entities';
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '123456789',
-    database: 'streamfi',
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: true,
     entities: [User, Campaign, Post, Score, XAccount, Continuation],
