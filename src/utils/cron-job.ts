412a794799c961ee45e81dd9ade8b428f72ccb87
Cyberdeus-ai <Cyberdeus.ai@icloud.com>
2025-09-16 02:43:21 +0800

    fix: Updated cron-job functions

--- src/utils/cron-job.ts
+++ src/utils/cron-job.ts
@@ -13,17 +13,17 @@ const getQuotesRepliesAndRTs = async () => {
   try {
     const tweetList = await getTweetListHandler();
     const engagerList = await getEngagerListHandler();
-    // if (tweetList && tweetList.length > 0) {
-    //   await fillReplyListHandler(tweetList, engagerList);
-    //   await fillRetweetListHandler(tweetList, engagerList);
-    //   await fillQuoteListHandler(tweetList, engagerList);
-    // }
+    if (tweetList && tweetList.length > 0) {
+      await fillReplyListHandler(tweetList, engagerList);
+      await fillRetweetListHandler(tweetList, engagerList);
+      await fillQuoteListHandler(tweetList, engagerList);
+    }
     return console.log("Cron Job is running!");
   } catch (err) {
     return console.error(err);
   }
 }
 
-export const cron = new CronJob('* * * * *', () => {
+export const cron = new CronJob('0 * * * *', () => {
   getQuotesRepliesAndRTs();
 });
